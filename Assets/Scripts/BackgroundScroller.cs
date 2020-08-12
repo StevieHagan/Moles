2a44d31e472f9aec937fcb3462e1832fcf34f323
Stephen Hagan <64257053+StevieHagan@users.noreply.github.com>
2020-08-12 21:12:29 +0100

    Music added. Improvements to home screen.

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float scrollSpeed = 0.5f;
+    [SerializeField] Material[] materials;
+
+    Renderer bgRenderer;
+
+    Vector2 offset;
+
+    void Start()
+    {
+        offset = new Vector2(0, scrollSpeed);
+        bgRenderer = GetComponent<Renderer>();
+
+        //Pick a background material at random
+        Material material = materials[Random.Range(0, materials.Length)];
+        bgRenderer.material = material;
+
+        //Rotate the background a random amount
+        gameObject.transform.rotation = Quaternion.Euler(0, 0,
+                                        Random.Range(0, 360));
+    }
+
+    void Update()
+    {
+        bgRenderer.material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        //Set this object up as a singleton
+
+        if(FindObjectsOfType<MusicPlayer>().Length > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
